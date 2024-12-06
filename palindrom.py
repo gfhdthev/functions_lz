eb3d16b43170817b35d130975f7465131172ca6d
gfhdthev <akkglow@tutamail.com>
2024-12-06 11:43:02 +0300

    v4

--- palindrom.py
+++ palindrom.py
@@ -2,16 +2,16 @@ user_input = str(input('Введите строку: '))
 
 #функция узнаем является ли число палиндромом
 def palindrom(b: str) -> bool:
-    b.lower()
+    b = b.lower()
 
     for i in b: #проверяем всю строку на пробелы
         if i == ' ': 
             b = b[:b.index(i)] + b[b.index(i)+1:] #вырезаем каждый пробел
 
-    b = b.split()
-
     for i in range(len(b)//2):
+
         if b[i] != b[-(i+1)]:
+
             return False #если два последних числа не совпадают, то выводит False
         
     return True #если жо этого не вывело False, то выведет True
@@ -19,5 +19,5 @@ def palindrom(b: str) -> bool:
 if palindrom(user_input) is True:
     print('Ваша строка палиндром')
 
-else:
+elif palindrom(user_input) is False:
     print('Ваша строка не палиндром')
